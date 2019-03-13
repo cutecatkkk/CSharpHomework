f5f104df26684fc8acc6a5764dcf39d7441fcf4f
Your Name <you@example.com>
2019-03-13 14:17:13 +0800

    homework3

---
+++ homework3/FactoryMethod/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorymethod
+{
+    public abstract class AuthCar
+    {
+        /// 输出制造了什么汽车
+        public abstract void CreatorCar();
+    }
+
+    /// 派生类-橘黄色轿车
+    public class _OriangeCar : AuthCar
+    {
+        public override void CreatorCar()
+        {
+            Console.WriteLine("生产了橘黄色轿车!");
+        }
+    }
+
+    /// 派生类-黑色轿车
+    public class _BlackCar : AuthCar
+    {
+        public override void CreatorCar()
+        {
+            Console.WriteLine("生产了黑色轿车");
+        }
+    }
+
+    /// 工厂类
+    public abstract class FactoryCreator
+    {
+        /// 获取汽车对象
+        public abstract AuthCar _AuthCar();
+    }
+
+    /// 得到黑色汽车实例
+    public class BlackCarFactoryMethod : FactoryCreator
+    {
+        public override AuthCar _AuthCar()
+        {
+            return new _BlackCar();
+        }
+    }
+
+    /// 得到橘黄色汽车实例
+    public class OriangeFactoryMethod : FactoryCreator
+    {
+        public override AuthCar _AuthCar()
+        {
+            return new _OriangeCar();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryCreator BlackCarfactoryCreator = new BlackCarFactoryMethod();
+            BlackCarfactoryCreator._AuthCar().CreatorCar();
+            FactoryCreator OriangefactoryCreator = new OriangeFactoryMethod();
+            OriangefactoryCreator._AuthCar().CreatorCar();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ homework3/Singleton/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*单例模式确保一个类只有一个实例,并提供一个全局访问点*/
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        //为了运行效率采用懒汉模式
+        private static Singleton uniqueinstance;
+        //为了实现多线程下的线程安全，使用互斥锁
+        private static readonly object locker = new object();
+        private Singleton()
+        { }
+
+        public static Singleton creatinstance()
+        {
+            //使用双重检测同步延迟加载去创建单例
+            if (uniqueinstance == null)
+            {
+                lock (locker)
+                {
+                    if (uniqueinstance == null)
+                    {
+                        uniqueinstance = new Singleton();
+                    }
+                }
+            }
+            return uniqueinstance;
+        }
+
+        public void write()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton a = Singleton.creatinstance();
+            a.write();
+            //创建b的时候因为uniqueinstance不为空，所以跳过了实例creatinstance，返回了和a一样的creatinstance
+            Singleton b = Singleton.creatinstance();
+            b.write();
+            Console.ReadKey();
+        }
+    }
+}
